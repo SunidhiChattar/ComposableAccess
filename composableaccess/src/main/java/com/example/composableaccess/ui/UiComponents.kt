1d9f3704b35919c5124ef458b52beb2681483fb6
Sunidhi Chattar <chattarsunidhi@gmail.com>
2023-07-19 13:31:07 +0530

    eight commit

--- composableaccess/src/main/java/com/example/composableaccess/ui/UiComponents.kt
+++ composableaccess/src/main/java/com/example/composableaccess/ui/UiComponents.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.unit.dp
 import com.example.composableaccess.R
 
